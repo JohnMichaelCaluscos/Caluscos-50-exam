270e17bfd9396efe1d839d6d799b5c2e44b6afab
JohnMichaelCaluscos <164433760+JohnMichaelCaluscos@users.noreply.github.com>
2024-03-26 13:29:10 +0800

    Add files via upload

---
+++ products_tbl (2).sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 26, 2024 at 06:23 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `product`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products_tbl`
+--
+
+CREATE TABLE `products_tbl` (
+  `Product_ID` varchar(200) NOT NULL,
+  `Product_Name` varchar(200) NOT NULL,
+  `Product_Price` varchar(200) NOT NULL,
+  `Product_Qty` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products_tbl`
+--
+
+INSERT INTO `products_tbl` (`Product_ID`, `Product_Name`, `Product_Price`, `Product_Qty`) VALUES
+('[0102]', '[laptop]', '[30000]', '[001]');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `products_tbl`
+--
+ALTER TABLE `products_tbl`
+  ADD PRIMARY KEY (`Product_ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
